417d3ed4bf34f4dcb18141f1db26656a66b40645
Gabriel Cosi <gabrielcosi18@gmail.com>
2020-01-16 10:27:14 -0500

    first commit

---
+++ src/main/java/com/foraneos/ForaneosApplication.java
@@ -0,0 +1,13 @@
+package com.foraneos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForaneosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForaneosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foraneos/ForaneosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foraneos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForaneosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
