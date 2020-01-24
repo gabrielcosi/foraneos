f9661fdd580b9ed660ab6e7128c363a845a68b2d
nickmeza <nickmeza@upeu.edu.pe>
2020-01-24 12:58:49 -0500

    Crud 22%

--- model/UniversidadModel.js
+++ model/UniversidadModel.js
@@ -3,6 +3,6 @@ const coordenadas = {longitud:String , latitud:String};
 var UniversidadEsquema = mongoose.Schema({
     nombre_largo:{type : String, require : true ,unique: true},
     nombre:{type : String, require : true ,unique: true},
-    coordenadas : coordenadas
+    coordenadas : {type : coordenadas, require : true ,unique: true}
 });
 module.exports = mongoose.model("universidad",UniversidadEsquema,"universidad");
